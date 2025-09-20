5efbbfeb1076c2b3a5d4d9814c4e8eee4657255a
Dunyushkin Nikita ISP-233 <124520018+Nadont02@users.noreply.github.com>
2025-09-20 12:35:04 +0300

    Update App.kt

--- src/App.kt
+++ src/App.kt
@@ -1,5 +1,53 @@
-fun main() {
-    val namber = readln().toInt()
-    println(namber)
+import kotlin.math.atan2
 
+fun main() {
+    //val namber = readln().toInt()
+    //println(namber)
+
+    /*print("Введите первое число: ")
+    val n1 = readln().toInt()
+    print("Введите второе число: ")
+    val n2 = readln().toInt()
+    if (n1 > n2){
+        println("первое число больше ")
+    }
+    else if (n1 == n2) {
+        println("равны")
+    }
+    else {
+        println("второе число больше ")
+    }
+    val max = if (n1 > n2) n1 else n2
+    println(max)*/
+    /*val age = readln().toInt()
+    val st = if (age >= 18) "Взрослый" else "ребенок"
+    println(st)*/
+
+    /*val n = 1 until 100
+    val n1 = 100 downTo 1 step 5
+    val s = "a".."z"
+    for(i in n) {
+        println(i)
+    }*/
+
+    /*print("Введите порядковый номер месяца 1-12 ")
+    val n = readln().toInt()
+    var month = when (n) {
+        1 ->  "Январь"
+        2 ->  "Февраль"
+        3 ->  "Март"
+        4 ->  "Апрель"
+        5 -> "май"
+        6 -> "Июнь"
+        7 ->  "Июль"
+        8 ->  "Август"
+        9 ->  "Сентябрь"
+        10 -> "Октябрь"
+        11 -> "Ноябрь"
+        12 ->  "Декабрь"
+        else -> "нет"
+    }
+    if ( n in 1..12){
+        println("Ваш индекс $n оотвествует месяцу $month")
+    }*/
 }
